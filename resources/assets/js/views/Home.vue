f814be86431521a6f85a5a05b7fae95d2889051d
Rodrigo Méndez <rodrigomendez@Rodrigos-MacBook-Pro.local>
2017-12-04 10:29:28 -0600

    Home vieew and project view created

---
+++ resources/assets/js/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <section id="featured" class="section-dark">
+      <div class="container">
+        <div class="row row-align-bottom margin-bottom--60">
+          <h4 class="col-3">Some of the projects we are most proud of.</h4>
+          <div class="col-9 text-right">
+            <button class="btn">See all</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4 col-no-padding">
+            <a href="" class="featured featured-plateiq reveal" data-color="#FCFCFD" data-text="The first driving simulator of its kind.">
+              <div class="featured-title">
+                <h5>Plate IQ</h5>
+                <h6>Restaurant Management</h6>
+                <div class="featured-title-description">
+                  <p>Read more.</p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-4 col-no-padding">
+            <a href="" class="featured featured-volkswagen text-dark reveal" data-color="#FCFCFD" data-text="The first driving simulator of its kind.">
+              <div class="featured-title">
+                <h5>Volkswagen 4DX</h5>
+                <h6>VR Driving Simulator</h6>
+                <div class="featured-title-description">
+                  <p>Read more.</p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-4 col-no-padding">
+            <a href="" class="featured featured-wolverine reveal" data-color="#FCFCFD" data-text="The first driving simulator of its kind.">
+              <div class="featured-title">
+                <h5>Wolverine</h5>
+                <h6>Mobile Configurator</h6>
+                <div class="featured-title-description">
+                  <p>Read more.</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+</div>
+</template>
+<script>
+export default {
+  
+}
+
+</script>
---
+++ resources/assets/js/views/Project.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <section id="whoweare" class="no-padding section-dark align-center component">
+    <div id="myNav" class="overlay">
+
+  <!-- Button to close the overlay navigation -->
+  <a href="javascript:void(0)" class="closebtn" >&times;</a>
+  </div>
+    <div class="container">
+      <div class="row">
+        <h1> project name</h1>
+        <h3 class="col-8 col-offset-2 text-center">project description.</h3>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script>
+
+
+  export default{
+
+  }
+</script>
