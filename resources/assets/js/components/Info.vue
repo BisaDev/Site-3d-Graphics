c45341c2dd31bde38ca61bcda379d15ad525214a
Rodrigo Méndez <rodrigomendez@Rodrigos-MacBook-Pro.local>
2017-12-04 10:45:09 -0600

    Header, Footer and Info component added

---
+++ resources/assets/js/components/Info.vue
@@ -0,0 +1,15 @@
+<template>
+<section id="whoweare" class="no-padding section-light align-center component">
+  <div class="container">
+    <div class="row">
+      <h3 class="col-8 col-offset-2 text-center">Maniak is a creative studio in Guadalajara, Mexico. We build websites, applications and interactive experiences.</h3>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+  export default{
+
+  }
+</script>
