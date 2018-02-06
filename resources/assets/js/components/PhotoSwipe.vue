5156a160150881929c78b73677d7d19e9448ffae
Jorge Rivera <jorge.r@maniak.com.mx>
2018-02-06 10:41:39 -0600

    Linting and prettier

--- resources/assets/js/components/PhotoSwipe.vue
+++ resources/assets/js/components/PhotoSwipe.vue
@@ -8,50 +8,59 @@
 
 
 <style>
-    .gallery-image { height: 300px; width: 500px; }
-    .gallery-image img { height: 100%; width: 100%; }
+.gallery-image {
+  height: 300px;
+  width: 500px;
+}
+.gallery-image img {
+  height: 100%;
+  width: 100%;
+}
 </style>
 
 <script>
-    import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default'
-    import PhotoSwipe from 'photoswipe/dist/photoswipe'
-    import $ from 'jquery'
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default'
+import PhotoSwipe from 'photoswipe/dist/photoswipe'
+import $ from 'jquery'
 
-    export default {
-        props: {
-            images: {
-                type: Array,
-                default: () => [],
-            },
-        },
-        data() {
-            return {
-                images_obj: this.images.map(function (el) {
-                    return {src: el, msrc: el, w: 1024, h: 615}
-                })
-            }
-        },
-        methods: {
-            openPhotoSwipe: function (index = 0) {
-                const pswpElement = this.$root.$refs.pswp;
-                const options = {
-                    getThumbBoundsFn: this.getThumbBoundsFn,
-                    history: false,
-                    index: index,
-                };
-
-                const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, this.images_obj, options);
-                gallery.init();
-            },
-            getThumbBoundsFn: function(index) {
-                let thumbnail = $(this.$el).children('.gallery-image').eq(index).get(0);
-                let rect = thumbnail.getBoundingClientRect();
-
-                return {x:rect.left, y:rect.top + $(window).scrollTop(), w:rect.width};
-            }
-        },
-        mounted() {
-            // this.openPhotoSwipe(1)
-        },
+export default {
+  props: {
+    images: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      images_obj: this.images.map(function(el) {
+        return { src: el, msrc: el, w: 1024, h: 615 }
+      }),
     }
+  },
+  methods: {
+    openPhotoSwipe: function(index = 0) {
+      const pswpElement = this.$root.$refs.pswp
+      const options = {
+        getThumbBoundsFn: this.getThumbBoundsFn,
+        history: false,
+        index: index,
+      }
+
+      const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, this.images_obj, options)
+      gallery.init()
+    },
+    getThumbBoundsFn: function(index) {
+      let thumbnail = $(this.$el)
+        .children('.gallery-image')
+        .eq(index)
+        .get(0)
+      let rect = thumbnail.getBoundingClientRect()
+
+      return { x: rect.left, y: rect.top + $(window).scrollTop(), w: rect.width }
+    },
+  },
+  mounted() {
+    // this.openPhotoSwipe(1)
+  },
+}
 </script>
--- resources/assets/js/views/Home.vue
+++ resources/assets/js/views/Home.vue
@@ -1,77 +1,77 @@
 <template>
 
-    <div id="home">
-        <home-header/>
-        <home-info/>
-        <photo-swipe :images="['../../img/featured/brightfox.png', '../../img/featured/plateiq.png']"/>
+  <div id="home">
+    <home-header/>
+    <home-info/>
+    <photo-swipe :images="['../../img/featured/brightfox.png', '../../img/featured/plateiq.png']"/>
 
-        <section class="home-featured">
-            <div class="container">
-                <div class="home-featured-title">
-                    <h3>Some of the projects<br>we are most proud of.</h3>
-                    <button class="button">See all projects</button>
-                </div>
-                <div class="home-featured-list">
-                    <router-link :key="project.id" :to="`/project/${project.id}`" class="project-preview dark reveal"
-                                 v-for="project in projects">
-                        <div class="project-preview-title">
-                            <h5>{{project.name}}</h5>
-                            <h6>{{project.description}}</h6>
+    <section class="home-featured">
+      <div class="container">
+        <div class="home-featured-title">
+          <h3>Some of the projects<br>we are most proud of.</h3>
+          <button class="button">See all projects</button>
+        </div>
+        <div class="home-featured-list">
+          <router-link :key="project.id" :to="`/project/${project.id}`" class="project-preview dark reveal"
+                       v-for="project in projects">
+            <div class="project-preview-title">
+              <h5>{{project.name}}</h5>
+              <h6>{{project.description}}</h6>
 
-                            <div class="project-preview-title-description">
-                                <p>Read more.</p>
-                            </div>
-                        </div>
-                    </router-link>
-
-                </div>
+              <div class="project-preview-title-description">
+                <p>Read more.</p>
+              </div>
             </div>
-        </section>
-    </div>
+          </router-link>
+
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 
 <script>
-    import homeHeader from '../components/home/Header';
-    import photoSwipe from '../components/PhotoSwipe';
-    import homeInfo from '../components/home/Info';
+import homeHeader from '../components/home/Header'
+import photoSwipe from '../components/PhotoSwipe'
+import homeInfo from '../components/home/Info'
 
-    import apiManiak from '../utils/api'
-    import Menu from '../utils/menu'
+import apiManiak from '../utils/api'
+import Menu from '../utils/menu'
 
-    export default {
-        beforeRouteLeave(to, from, next) {
-            this.$root.$refs.revealer.reveal().then(() => next())
-        },
-        props: {
-            reveal: {
-                default: false,
-                type: Boolean,
-            },
-        },
+export default {
+  beforeRouteLeave(to, from, next) {
+    this.$root.$refs.revealer.reveal().then(() => next())
+  },
+  props: {
+    reveal: {
+      default: false,
+      type: Boolean,
+    },
+  },
 
-        components: {
-            homeHeader,
-            photoSwipe,
-            homeInfo,
-        },
+  components: {
+    homeHeader,
+    photoSwipe,
+    homeInfo,
+  },
 
-        data() {
-            return {
-                projects: []
-            };
-        },
-
-        mounted() {
-            apiManiak.getProjects().then(this.updateData);
-            Menu.run()
-        },
-
-        methods: {
-            updateData(response) {
-                this.projects = response.data;
-                this.$root.$refs.revealer.close()
-            },
-        },
+  data() {
+    return {
+      projects: [],
     }
+  },
+
+  mounted() {
+    apiManiak.getProjects().then(this.updateData)
+    Menu.run()
+  },
+
+  methods: {
+    updateData(response) {
+      this.projects = response.data
+      this.$root.$refs.revealer.close()
+    },
+  },
+}
 </script>
