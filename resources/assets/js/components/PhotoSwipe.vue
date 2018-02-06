633a2af8d85c10edf05d3d1476704e2e200ed48d
Jorge Rivera <jorge.r@maniak.com.mx>
2018-02-06 12:42:28 -0600

    Linting Removed jQuery

--- resources/assets/js/components/PhotoSwipe.vue
+++ resources/assets/js/components/PhotoSwipe.vue
@@ -1,66 +1,62 @@
 <template>
-    <section class="ps-gallery">
-        <div v-for="(image, index) in images" v-on:click="openPhotoSwipe(index)" class="gallery-image">
-            <img :src="image" />
-        </div>
-    </section>
+  <section class="ps-gallery">
+    <div ref="galleryImage" v-for="(image, index) in images" @click="openPhotoSwipe(index)" class="gallery-image">
+      <img :src="image"/>
+    </div>
+  </section>
 </template>
 
 
 <style>
-.gallery-image {
-  height: 300px;
-  width: 500px;
-}
-.gallery-image img {
-  height: 100%;
-  width: 100%;
-}
+  .gallery-image {
+    height: 300px;
+    width: 500px;
+  }
+
+  .gallery-image img {
+    height: 100%;
+    width: 100%;
+  }
 </style>
 
 <script>
-import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default'
-import PhotoSwipe from 'photoswipe/dist/photoswipe'
-import $ from 'jquery'
+  import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default'
+  import PhotoSwipe from 'photoswipe/dist/photoswipe'
 
-export default {
-  props: {
-    images: {
-      type: Array,
-      default: () => [],
+  export default {
+    props: {
+      images: {
+        type: Array,
+        default: () => [],
+      },
     },
-  },
-  data() {
-    return {
-      images_obj: this.images.map(function(el) {
-        return { src: el, msrc: el, w: 1024, h: 615 }
-      }),
-    }
-  },
-  methods: {
-    openPhotoSwipe: function(index = 0) {
-      const pswpElement = this.$root.$refs.pswp
-      const options = {
-        getThumbBoundsFn: this.getThumbBoundsFn,
-        history: false,
-        index: index,
+    data() {
+      return {
+        images_obj: this.images.map(function (el) {
+          return {src: el, msrc: el, w: 1024, h: 615}
+        }),
       }
-
-      const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, this.images_obj, options)
-      gallery.init()
     },
-    getThumbBoundsFn: function(index) {
-      let thumbnail = $(this.$el)
-        .children('.gallery-image')
-        .eq(index)
-        .get(0)
-      let rect = thumbnail.getBoundingClientRect()
+    methods: {
+      openPhotoSwipe(index = 0) {
+        const pswpElement = this.$root.$refs.pswp
+        const options = {
+          getThumbBoundsFn: this.getThumbBoundsFn,
+          history: false,
+          index: index,
+        }
 
-      return { x: rect.left, y: rect.top + $(window).scrollTop(), w: rect.width }
+        console.log(this.$refs)
+        const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, this.images_obj, options)
+        gallery.init()
+      },
+      getThumbBoundsFn(index) {
+        const thumbnail = this.$refs.galleryImage[index]
+        const pageYScroll = window.pageYOffset || document.documentElement.scrollTop
+        const rect = thumbnail.getBoundingClientRect()
+
+        return {x: rect.left, y: rect.top + pageYScroll, w: rect.width}
+      },
     },
-  },
-  mounted() {
-    // this.openPhotoSwipe(1)
-  },
-}
+  }
 </script>
