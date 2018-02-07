90af268f4a2e632a0e8ebec05e3f2c8ff23a0e83
Jorge Rivera <jorge.r@maniak.com.mx>
2018-02-06 19:02:32 -0600

    added a top class for nav

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -5,68 +5,82 @@
         <img :src="logo" class="logo-maniak" alt="MANIAK">
       </router-link>
       <ul class="navbar-menu">
-        <li><router-link to="/work">Work</router-link></li>
-        <li><router-link to="/studio">Studio</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
+        <li>
+          <router-link to="/work">Work</router-link>
+        </li>
+        <li>
+          <router-link to="/studio">Studio</router-link>
+        </li>
+        <li>
+          <router-link to="/contact">Contact</router-link>
+        </li>
       </ul>
 
-</div>
-</nav>
+    </div>
+  </nav>
 </template>
 
 <style>
-.navbar {
-  transition: transform 0.5s ease;
-}
-.nav-show {
-  transform: translateY(0%);
-}
-.nav-hide {
-  transform: translateY(-100%);
-}
+  .navbar {
+    transition: transform 0.5s ease;
+  }
+
+  .nav-show {
+    transform: translateY(0%);
+  }
+
+  .nav-hide {
+    transform: translateY(-100%);
+  }
 </style>
 
 <script>
-export default {
-  data() {
-    return {
-      logo: require('../../img/logos/maniak/logo-maniak-dark.svg'),
-      lastKnownScrollPosition: window.scrollY,
-      ticking: false,
-      stateClass: 'nav-show',
-      theme: 'light',
-      offset: 5, //px
-    }
-  },
-
-  //mounted() {},
-
-  created() {
-    window.addEventListener('scroll', this.trackScroll)
-  },
-
-  destroyed() {
-    window.removeEventListener('scroll', this.trackScroll)
-  },
-  methods: {
-    trackScroll() {
-      if (!this.ticking) {
-        window.requestAnimationFrame(this.modifyNav)
+  export default {
+    data() {
+      return {
+        logo: require('../../img/logos/maniak/logo-maniak-dark.svg'),
+        lastKnownScrollPosition: window.scrollY,
+        ticking: false,
+        stateClass: 'nav-show',
+        theme: 'light',
+        offset: 5, //px
       }
-
-      this.ticking = true
     },
 
-    modifyNav() {
-      if (window.scrollY >= this.lastKnownScrollPosition + this.offset) {
-        this.stateClass = 'nav-hide'
-      } else if (window.scrollY <= this.lastKnownScrollPosition - this.offset) {
-        this.stateClass = 'nav-show'
-      }
-      this.lastKnownScrollPosition = window.scrollY
-
-      this.ticking = false
+    created() {
+      window.addEventListener('scroll', this.trackScroll)
     },
-  },
-}
+
+    destroyed() {
+      window.removeEventListener('scroll', this.trackScroll)
+    },
+    methods: {
+      trackScroll() {
+        if (!this.ticking) {
+          window.requestAnimationFrame(this.modifyNav)
+        }
+
+        this.ticking = true
+      },
+
+      modifyNav() {
+
+        const pageYScroll = window.pageYOffset || document.documentElement.scrollTop
+        if (pageYScroll < 5) {
+          this.stateClass = 'nav-at-top'
+        } else {
+          this.stateClass = ''
+          if (window.scrollY >= this.lastKnownScrollPosition + this.offset) {
+            this.stateClass = 'nav-hide'
+          } else if (window.scrollY <= this.lastKnownScrollPosition - this.offset) {
+            this.stateClass = 'nav-show'
+          }
+        }
+
+        this.lastKnownScrollPosition = window.scrollY
+
+        this.ticking = false
+      },
+    },
+  }
 </script>
