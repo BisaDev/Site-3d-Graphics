86777b9b9964b0398b3d3a0a45543fa2890bcf1c
Christian Reindl <reindl@maniak.com.mx>
2018-02-07 18:23:46 -0600

    Update home visual

--- resources/assets/js/components/home/Header.vue
+++ resources/assets/js/components/home/Header.vue
@@ -1,44 +1,56 @@
 <template>
-  <section class="home-header no-padding fullscreen no-margin">
+  <section class="home-header no-padding fullscreen no-margin" ref="section">
     <div class="home-header-background"></div>
-    <div class="home-header-visual">
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-      <defs>
-        <filter id="goo">
-          <feGaussianBlur in="SourceGraphic" stdDeviation="12" result="blur" />
-          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
-          <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
-        </filter>
-      </defs>
-      <g class="blob" filter="url(#goo)">
-        <circle cx="50%" cy="50%" r="19%" fill="#1581F9" />
-        <circle cx="55%" cy="55%" r="22%" fill="#1581F9" />
-        <circle cx="52%" cy="42%" r="21%" fill="#1581F9" />
-      </g>
-      <g class="blob" filter="url(#goo)">
-        <circle cx="53%" cy="52%" r="20%" fill="#586170" />
-        <circle cx="53%" cy="56%" r="20%" fill="#586170" />
-        <circle cx="56%" cy="43%" r="19%" fill="#586170" />
-      </g>
-      <g class="blob" filter="url(#goo)">
-        <circle cx="55%" cy="49%" r="20%" fill="#D2003E" />
-        <circle cx="50%" cy="54%" r="18%" fill="#D2003E" />
-        <circle cx="46%" cy="41%" r="22%" fill="#D2003E" />
-      </g>
-    </g>
-    </svg>
+    <div class="home-header-explosion" ref="explosion"></div>
+    <div class="home-header-visual" ref="visual">
+      <div class="home-header-visual-svg-wrap">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="9" result="blur" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+              <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
+            </filter>
+          </defs>
+          <g class="blob" filter="url(#goo)">
+            <circle cx="50%" cy="50%" r="90" fill="#1581F9" />
+            <circle cx="54%" cy="51%" r="90" fill="#1581F9" />
+            <circle cx="53%" cy="48%" r="80" fill="#586170" />
+            <circle cx="49%" cy="56%" r="80" fill="#586170" />
+            <circle cx="55%" cy="49%" r="80" fill="#D2003E" />
+            <circle cx="47%" cy="52%" r="80" fill="#D2003E" />
+          </g>
+        </g>
+      </svg>
     </div>
-    <div class="container text-center dark">
-        <h1>
-          Let us blow your mind.
-        </h1>
-        <button class="button button-scroll">
-          <span>Scroll down</span><span>Scroll down</span>
-        </button>
-    </div>
-  </section>
+  </div>
+  <div class="container text-center dark">
+    <h1>
+      Let us blow your mind.
+    </h1>
+    <button class="button button-scroll">
+      <span>Scroll down</span><span>Scroll down</span>
+    </button>
+  </div>
+</section>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    const explosion = this.$refs.explosion
+    const visual = this.$refs.visual
+    const section = this.$refs.section
+
+    visual.addEventListener('click', function() {
+      visual.classList.add('hidden')
+      explosion.classList.add('show')
+      section.classList.toggle('dark')
+      setTimeout(function() {
+        visual.classList.remove('hidden')
+        explosion.classList.remove('show')
+      }, 2000)
+    })
+  },
+}
 </script>
