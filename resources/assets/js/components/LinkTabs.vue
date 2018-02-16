922026f31849010850ffe0d18c8b156624a57134
Jorge Rivera <jorge.r@maniak.com.mx>
2018-02-16 14:30:47 -0600

    WIP: project tabs

---
+++ resources/assets/js/components/LinkTabs.vue
@@ -0,0 +1,34 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    import _ from 'lodash'
+    export default {
+        props: {
+            projects: {
+                type: Array,
+                required: true,
+            },
+            keyToGroup: {
+                type: String,
+                required: true,
+            },
+        },
+
+        computed: {
+            tabs() {
+                return _.groupBy(this.projects, element => element[this.keyToGroup])
+            }
+        },
+        mounted() {
+            console.log( _.groupBy(this.projects, element => element[this.keyToGroup]))
+            console.log( this.projects )
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/Tabs.vue
@@ -0,0 +1,46 @@
+<template>
+    <div calss="tabs">
+        <div class="container work-header-filters-container">
+            <ul class="work-header-filters-list">
+                <li><a class="active" href="">All<span class="filter-count">12</span></a></li>
+            </ul>
+        </div>
+        <section class="work-portfolio">
+            <div class="container">
+                <div class="grid">
+                    <router-link v-for="project in projects" :to="" class="preview reveal"
+                       :style="{backgroundColor: '#e7e9f0', backgroundImage: 'project.'}">
+                        <div class="preview-title">
+                            <h5>Wolverine</h5>
+                            <h6>Online Configurator</h6>
+                            <div class="preview-title-cta">
+                                <p>Read more.</p>
+                            </div>
+                        </div>
+                    </router-link>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            content: {
+                type: Object,
+                required: true,
+            }
+        },
+        data() {
+          return {
+              tabs: Object.keys(this.content),
+              projects: Object.values(this.content),
+          }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
