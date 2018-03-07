da549b6249dbc6d45ef8fac38acf3cb9b041fb15
Jorge Rivera <jorge.r@maniak.com.mx>
2018-03-07 13:08:09 -0600

    Hotfix: p on single text section

--- resources/assets/js/components/project/ProjectTextInformation.vue
+++ resources/assets/js/components/project/ProjectTextInformation.vue
@@ -1,20 +1,25 @@
 <template>
     <div class="container">
         <div class="grid">
-            <div class="project-copy-text" v-html="body">
-
+            <div class="project-copy-text">
+                <h4 v-html="title" v-if="title"></h4>
+                <p v-html="body"></p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-  props: {
-    body: {
-      type: String,
-      required: true,
-    },
-  },
-}
+    export default {
+        props: {
+            body: {
+                type: String,
+                required: true,
+            },
+            title: {
+                type: String,
+                default: '',
+            },
+        },
+    }
 </script>
