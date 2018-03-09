b3339fa4e4ecb421c7ac17ebeacec36ec1489cae
Jorge Rivera <jorge.r@maniak.com.mx>
2018-03-09 14:35:50 -0600

    Removed project id from factory

--- database/factories/ProjectQuote.php
+++ database/factories/ProjectQuote.php
@@ -7,9 +7,6 @@ use App\ProjectQuote;
 
 $factory->define(ProjectQuote::class, function (Faker $faker) {
     return [
-        'project_id' => function () {
-            return factory(Project::class)->create()->id;
-        },
         'body' => $faker->text(300),
         'author' => $faker->name(),
         'title' => $faker->text(191),
