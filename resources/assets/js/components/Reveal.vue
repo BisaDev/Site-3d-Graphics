121cb742eedc71ed009cf9530ad7f950adf39fa3
Christian Reindl <reindl@maniak.com.mx>
2018-03-26 12:56:12 -0600

    New revealer phrases

--- resources/assets/js/components/Reveal.vue
+++ resources/assets/js/components/Reveal.vue
@@ -21,10 +21,10 @@
             phrases: {
                 type: Array,
                 default: () => [
-                    'We have an insatiable curiosity for big ideas and love making them happen.',
-                    'A controlled kind of crazy with a passion for creative disruption.',
-                    'Thinking about cool transition phrases... 🤔',
-                    'This is nice -_-',
+                  'We have an insatiable curiosity for big ideas and love making them happen.',
+                  'A controlled kind of crazy with a passion for creative disruption.',
+                  'Maniac, [ˈmā-nē-ˌak]: A person characterized by an unbridled enthusiasm for something.',
+                  'Thinking about cool transition phrases... 🤔',
                 ],
             },
         },
