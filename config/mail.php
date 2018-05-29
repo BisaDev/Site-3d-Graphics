ccb5fe2aab843d0e677f961c8aee635742d1c6e3
Hector Garcia <hector@maniak.com.mx>
2018-05-28 22:40:46 -0500

    HOTFIX: Reverts mail.php default values due to server error

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'web@maniak.co'),
-        'name' => env('MAIL_FROM_NAME', 'Maniak Website'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
