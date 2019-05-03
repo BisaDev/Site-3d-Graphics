3b9e4f5429b39088bc3897ad912b40b1bb9d118e
Christian Reindl <reindl@maniak.com.mx>
2019-05-03 17:07:09 -0500

    Change footer

--- resources/assets/js/components/Footer.vue
+++ resources/assets/js/components/Footer.vue
@@ -1,17 +1,48 @@
 <template>
     <footer>
-        <section class="dark">
-            <a href="mailto:hello@maniak.co" class="container">
-                <div class="grid">
-                    <h4 class="footer-title">
-                        <span>Lets blow minds together.</span>
-                        <span>👉&nbsp;&nbsp;hello@maniak.co&nbsp;&nbsp;👈</span>
-                    </h4>
-                </div>
+      <div class="footer-top">
+        <div class="container">
+          <div class="grid">
+          <div class="footer-contact">
+            <router-link class="footer-logo" to="/">
+              <logo></logo>
+            </router-link>
+            <h4 class="footer-contact-copy">
+              If you’d like to partner on a project or simply chat about what we could do together, just say <a class="contact-email" href="mailto: hello@maniak.co">hello@maniak.co</a>.
+            </h4>
+          </div>
+          <ul class="footer-links">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/work">Work</router-link></li>
+            <li><router-link to="/studio">Studio</router-link></li>
+            <li><router-link to="/contact">Contact</router-link></li>
+            <li><router-link to="/news">News</router-link></li>
+          </ul>
+          <ul class="footer-links">
+            <li><a href="https://www.facebook.com/themaniaks/" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+            <li><a href="https://www.instagram.com/themaniaks/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+            <li><a href="https://www.linkedin.com/company/maniak" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li><a href="https://twitter.com/maniakbtl" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+          </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <div class="container footer-bottom-container">
+            <a href="https://www.mnk.rocks" rel="noopener noreferrer" class="footer-mnk-logo">
+              <img src="/img/logos/mnk/logo-mnk-black.svg" alt="mnk">
             </a>
-        </section>
+            <p class="footer-mnk-description">We are part of the mnk studio collective. <a href="https://www.mnk.rocks" rel="noopener noreferrer">Learn more</a></p>
+        </div>
+      </div>
     </footer>
 </template>
 <script>
-    export default {}
+import Logo from './Logo'
+
+export default {
+    components: {
+        Logo,
+    },
+}
 </script>
