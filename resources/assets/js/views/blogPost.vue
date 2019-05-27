1723ab1ea6996003c411245637ac5c45136b1a4b
Hector Garcia <hector@maniak.com.mx>
2019-05-27 09:30:10 -0500

    Changes to maniak time copy

--- resources/assets/js/views/blogPost.vue
+++ resources/assets/js/views/blogPost.vue
@@ -7,7 +7,7 @@
           </h1>
         </div>
         <div class="appear-after-reveal appear-after-reveal-delay">
-          <h1 class="blog-post-header-subtitle title--large">Our New Working Hours Policy.</h1>
+          <h1 class="blog-post-header-subtitle title--large">Our New Improvement Policy.</h1>
         </div>
       </div>
     </section>
@@ -25,24 +25,24 @@
               <div class="blog-post-content-callout-schedule">
                 <div class="blog-post-content-callout-schedule-column">
                   <div class="blog-post-content-callout-subtitle">Mon – Thu</div>
-                  <div class="blog-post-content-callout-subtitle-small">9am – 6pm</div>
+                  <div class="blog-post-content-callout-subtitle-small">9am – 6pm (CST)</div>
                 </div>
                 <div class="blog-post-content-callout-schedule-column">
                   <div class="blog-post-content-callout-subtitle">Fri</div>
-                  <div class="blog-post-content-callout-subtitle-small">9am – 12pm</div>
+                  <div class="blog-post-content-callout-subtitle-small">9am – 12pm (CST)</div>
                 </div>
               </div>
             </div>
           </div>
           <div class="blog-post-content-main">
             <p>
-              In an industry as competitive as ours ( Software & Design services ) we need to constantly change the way we do things, experiment with new methods, new technologies and have the willingness to invest in a path we deem will bring the best out of our work, will increase our client satisfaction and value of what we deliver as a Company.
+              In an industry as competitive as the software and design industry  we need to constantly change the way we do things. The way in which we experiment with new methods, new technologies and have the willingness to invest in a path we deem will bring the best out of our work, will increase our client satisfaction and value of what we deliver as a Company.
             </p>
             <p>
-              However it’s also a reality that trying to find the time to improve as individuals and as a Company , is to say the least, a struggle. Not because we do not want to, or because we are not disciplined but because we live for our work, we are passionate and relentless in our efforts to achieve our goals and we always want to do better and that attitude sometimes takes the best out of us.
+              However, it’s also a reality that trying to find the time to improve as individuals and as a company, is, to say the least, a struggle. Not because of a lack of motivation and discipline but because we live for our work. We are passionate and relentless in our efforts to achieve our goals and we always want to do better. This attitude sometimes takes the best out of us.
             </p>
             <p>
-              That is why we have decided to institute a new company policy called “Maniak Time” that will allow us to keep perfecting our craft with activities targeted to increase our value and improve as professionals. The rules of the game are simple, in order to improve we need time and that time will be taken out of every Friday.  This means that our consulting hours will be Monday to Thursday from 9 to 6, and Fridays from 9 to 12.             </p>
+              That is why we have decided to institute a new company policy called “Maniak Time” that will allow us to keep perfecting our craft with activities targeted to increase our value and improve as professionals. The rules of the game are simple: We need time and that time will be taken out of every Friday.  This means that our consulting hours will be Monday to Thursday from 9 AM to 6 PM, and Fridays from 9 AM to 12 PM.
           </div>
         </div>
       </div>
@@ -58,7 +58,7 @@
           </div>
           <div class="blog-post-content-main">
             <p>
-              We take this change very seriously and we firmly believe that the outcome will reap an immediate positive change in the way we do business, how we handle projects and in the quality of our work. It will force us to be more effective and punctual in all of our actions.            </p>
+              We take this change very seriously and we firmly believe that the outcome will reap an immediate positive change in the way we do business, how we handle projects and in the quality of our work. It will force us to be more effective and punctual in all of our actions.
           </div>
         </div>
       </div>
