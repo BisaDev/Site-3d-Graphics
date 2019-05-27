98a8ed666d1fb38551525986ceec5f760b9a1249
Christian Reindl <reindl@maniak.com.mx>
2019-05-27 12:20:42 -0500

    add back comment and remove debugging code

--- resources/assets/js/views/blogPost.vue
+++ resources/assets/js/views/blogPost.vue
@@ -64,7 +64,21 @@
           </div>
         </div>
       </div>
-
+      <!--      <div class="blog-post-content-section container container&#45;&#45;small">-->
+      <!--        <div class="grid">-->
+      <!--          <div class="blog-post-content-callout">-->
+      <!--            <h2 class="blog-post-content-callout-title">To everyone at Maniak.</h2>-->
+      <!--          </div>-->
+      <!--          <div class="blog-post-content-main">-->
+      <!--            <p>-->
+      <!--              We want to be better as a group and individuals, we want to be the best in the industry, now we have the time to improve and achieve that.-->
+      <!--            </p>-->
+      <!--            <p>-->
+      <!--              This new program / policy is something that if we take advantage of, will push us ahead of the competition but it’s also something that can be abused. We trust every one of you, we trust that you will use the time wisely .-->
+      <!--            </p>-->
+      <!--          </div>-->
+      <!--        </div>-->
+      <!--      </div>-->
     </section>
     <section class="blog-post-instagram">
       <div class="container container--small">
@@ -127,7 +141,6 @@ export default {
                 </div>`,
             target: 'blog-post-instagram-feed',
             after: function() {
-                console.log('test')
                 // disable button if no more results to load
                 if (!this.hasNext()) {
                     $this.$refs.buttonLoadMore.style.display = 'none'
