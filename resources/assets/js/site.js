4a79f6e73f3cb4bb570f5954857a14e0e481d8e0
wendy <suleyma@maniak.com.mx>
2019-10-03 16:41:54 -0500

    Hotfix: Remove window.chrome validation for showing error message

--- resources/assets/js/site.js
+++ resources/assets/js/site.js
@@ -13,7 +13,7 @@ import './bootstrap'
 new Vue({
     el: '#app',
     router,
-    components: {navbar, reveal, footerComponent, deathScreen},
+    components: { navbar, reveal, footerComponent, deathScreen },
     data() {
         return {
             navTheme: 'light',
@@ -24,10 +24,10 @@ new Vue({
             revealerIsAnimating: true,
             revealerIsOpen: true,
             defaultPhrases: [
-              'We have an insatiable curiosity for big ideas and love making them happen.',
-              'A controlled kind of crazy with a passion for creative disruption.',
-              'Maniac, [ˈmā-nē-ˌak]: A person characterized by an unbridled enthusiasm for something.',
-              'Thinking about cool transition phrases... 🤔',
+                'We have an insatiable curiosity for big ideas and love making them happen.',
+                'A controlled kind of crazy with a passion for creative disruption.',
+                'Maniac, [ˈmā-nē-ˌak]: A person characterized by an unbridled enthusiasm for something.',
+                'Thinking about cool transition phrases... 🤔',
             ],
             // initial phrase
             phrases: [''],
@@ -65,8 +65,14 @@ new Vue({
 
                 if (this.death) {
                     window.console.clear()
-                    window.console.log('%cHello there, so you like looking under the hood?', 'font-size:40px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15)')
-                    window.console.info('%cthere might be something else here', 'font-size:10px;color:#FF1141')
+                    window.console.log(
+                        '%cHello there, so you like looking under the hood?',
+                        'font-size:40px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15)'
+                    )
+                    window.console.info(
+                        '%cthere might be something else here',
+                        'font-size:10px;color:#FF1141'
+                    )
                 }
             }
         },
@@ -75,11 +81,13 @@ new Vue({
             if (email) {
                 this.wantsJob = true
                 window.console.clear()
-                window.console.log('%cNice! we will be in touch', 'font-size:40px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15)')
+                window.console.log(
+                    '%cNice! we will be in touch',
+                    'font-size:40px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15)'
+                )
             } else {
                 window.console.log('maybe send me your email')
             }
-
         },
     },
 
@@ -89,16 +97,19 @@ new Vue({
         window.job = this.requestJob
 
         // this will only run when console is open
-        this.$refs.consoleChecker.__defineGetter__("id", () => {
+        this.$refs.consoleChecker.__defineGetter__('id', () => {
             this.devtoolsOpen = true
         })
         // this will only run when console is open (chrome 65+)
         this.consoleChecker.toString = () => {
-            if(window.chrome) {
+            /*HOTFIX: This line is getting an object in newest versions of
+            chrome so it always displays the error screen even when the
+            console is not open. Please check later for a better solution.*/
+            /*if (window.chrome) {
                 this.devtoolsOpen = true
-            }
+            }*/
         }
 
-        this.enableDeath && setInterval(this.killIt, 1000)
-    }
+        setInterval(this.killIt, 1000)
+    },
 })
