3611d094d7c94d17ffb7429c9bd4f505b3fc9575
wendy <suleyma@maniak.com.mx>
2019-12-05 14:59:49 -0600

    Remove duplicated function

--- resources/assets/js/siteRouter.js
+++ resources/assets/js/siteRouter.js
@@ -137,32 +137,6 @@ const router = new Router({
     mode: 'history',
 })
 
-
-// This callback runs before every route change, including on page load.
-// Check this link: https://alligator.io/vuejs/vue-router-modify-head/
-router.beforeEach((to, from, next) => {
-  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
-  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
-  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
-  if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
-
-  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
-
-  if(!nearestWithMeta) return next();
-  nearestWithMeta.meta.metaTags.map(tagDef => {
-    const tag = document.createElement('meta');
-
-    Object.keys(tagDef).forEach(key => {
-      tag.setAttribute(key, tagDef[key]);
-    });
-    tag.setAttribute('data-vue-router-controlled', '');
-    return tag;
-  })
-  .forEach(tag => document.head.appendChild(tag));
-  next();
-});
-
-
 router.beforeEach((to, from, next) => {
     if (!to.matched.length) {
         next('/notFound')
